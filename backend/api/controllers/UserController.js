ab3a88388f2164856918f659dbdfe569d5fbac33
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-03-25 15:46:36 -0600

    Implementar validación de modelo con Joi

--- backend/api/controllers/UserController.js
+++ backend/api/controllers/UserController.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 /**
  * UserController
  *
@@ -6,15 +8,27 @@
  */
 
 module.exports = {
-  
-
   /**
    * `UserController.signup()`
    */
   signup: async function (req, res) {
-    return res.json({
-      todo: 'signup() is not implemented yet!'
-    });
+    try {
+      const schema = Joi.object().keys({
+        email: Joi
+          .string()
+          .required()
+          .email(),
+        password: Joi.string().required()
+      });
+      const params = await schema.validate(req.allParams());
+      return res.ok(params);
+
+    } catch (error) {
+      if (error.name === 'ValidationError') {
+        return res.badRequest({ error }).json();
+      }
+      return res.serverError({ error }).json();
+    }
   },
 
   /**
@@ -25,6 +39,4 @@ module.exports = {
       todo: 'login() is not implemented yet!'
     });
   }
-
-};
-
+}
